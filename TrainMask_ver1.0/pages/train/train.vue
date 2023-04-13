4d5d2266c8575ca1a47fb3709401694002857c8e
nanami7 <66000847+Yuuki-Wu@users.noreply.github.com>
2023-04-14 00:42:52 +0800

    Add files via upload

---
+++ TrainMask_ver1.0/pages/train/train.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		开始训练
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>

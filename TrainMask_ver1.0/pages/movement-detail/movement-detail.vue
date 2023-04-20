8b5182b341c444e34b68eb0c2b8d46165d5e0868
Yuuki-Wu <1600345769@qq.com>
2023-04-21 03:36:53 +0800

    detail
    Signed-off-by: Yuuki-Wu <1600345769@qq.com>

---
+++ TrainMask_ver1.0/pages/movement-detail/movement-detail.vue
@@ -0,0 +1,45 @@
+<template>
+	<view>
+		<view class="top">
+			<image :src="url"></image>
+		</view>
+		<view class="bottom">
+			<view class="tit">
+				<view class="main-point">
+					<view class="note"></view>
+					<view class="detail">
+						<text>{{ this.detail }}</text>
+					</view>
+					<view class="steps">
+						<text>{{ this.steps}}</text>
+					</view>
+				</view>
+				<view class="history"></view>
+				<view class="graph"></view>
+			</view>
+
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		components: {},
+		name: 'detail',
+		data(){
+			return{
+				url:'',
+				detail:'',
+				steps:''
+			}
+		},
+		onLoad:function(option){
+			this.url = option.Img
+			this.detail = option.detail
+			this.steps = option.steps
+		}
+	}
+</script>
+
+<style>
+</style>
